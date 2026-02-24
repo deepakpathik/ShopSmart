2ee59b55fffc50b25665285ff0c1b36d3bf66a13
deepakpathik <deepakpathik2005@gmail.com>
2026-02-24 10:00:00 +0530

    fix: update broken css import in app

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
-import './index.css';
+import './styles.css';
 
 const App = () => {
   return (
